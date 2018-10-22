14972d0b5adbf390a75bc306204f5e0d575a2119
cristina <cristina@192.168.1.128>
2018-10-22 19:42:10 +0200

    rama numericos

--- src/basicoscristina/BasicosCristina.java
+++ src/basicoscristina/BasicosCristina.java
@@ -10,12 +10,36 @@ package basicoscristina;
  * @author cristina
  */
 public class BasicosCristina {
+       /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
 
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        numericos();  // Llamada al método
+    }
     }
     
-}
+
